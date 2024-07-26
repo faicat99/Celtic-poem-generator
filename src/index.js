8a29c9d2538ed09cab00526d020bfecbf86a826c
Fainche <faicatmck@gmail.com>
2024-07-26 21:28:42 +0100

    improved the user experience; Modified HTML by adding a hint, improved JS by adding a generating a message and hiding the poem box for it to appear once topic searched. Added blink animation for searching effect

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generatePoem(event) {
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "bafb81c036f1dc4bfbb21532bb2ot295";
-    let prompt = `User instructions: Generate a poem from Ireland about ${instructionsInput.value} in Irish language with english translation.`;
-    let context = "In basic HTML write 1 short poem. seperate each line with a <br />. The english translation after the Irish translation has finished. Follow user instructions";
+    let prompt = `User instructions: Generate a poem Celtic poem about ${instructionsInput.value} in Irish language with english translation.`;
+    let context = "In basic HTML write 1 short poem. seperate each line with a <br />. The english translation after the Irish translation has finished. At the bottom have Fainche AI in strong and green. Follow user instructions";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳Generating the Celtic Poem about ${instructionsInput.value}...</div>`;
     
     console.log("Generating Poem");
     console.log(`Prompt: ${prompt}`);
