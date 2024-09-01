00eee264d28c02741aa5ae1e5e6d792cbd7d840b
Fainche <faicatmck@gmail.com>
2024-09-01 19:07:18 +0100

    updated some of styling and html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 function displayPoem(response) {
-    new Typewriter("#poem", {
+    new Typewriter("#translator", {
         strings: response.data.answer,
         autoStart: true,
         delay: 1,
@@ -12,16 +12,16 @@ function generatePoem(event) {
 
     let instructionsInput = document.querySelector("#user-instructions");
     let apiKey = "bafb81c036f1dc4bfbb21532bb2ot295";
-    let prompt = `User instructions: Generate a poem Celtic poem about ${instructionsInput.value} in Irish language with english translation.`;
-    let context = "In basic HTML write 1 short poem. seperate each line with a <br />. The english translation after the Irish translation has finished. At the bottom have Fainche AI in strong and green. Follow user instructions";
+    let prompt = `User instructions: Generate a translation about ${instructionsInput.value} in the Irish Language. Show the english translation after the Irish translation`;
+    let context = "In basic HTML write language translation that user has requested. Seperate each line with a <br />. At the bottom have Fainche AI in strong and green. Follow user instructions";
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    let poemElement = document.querySelector("#poem");
+    let poemElement = document.querySelector("#translator");
     poemElement.classList.remove("hidden");
-    poemElement.innerHTML = `<div class="generating">⏳Generating the Celtic Poem about ${instructionsInput.value}...</div>`;
+    poemElement.innerHTML = `<div class="generating">⏳Generating the translation about ${instructionsInput.value}...</div>`;
     
     axios.get(apiUrl).then(displayPoem);
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
+let poemFormElement = document.querySelector("#translator-form");
 poemFormElement.addEventListener("submit", generatePoem);
